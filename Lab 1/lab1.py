83933ade11bc46467d6084315c230586c9c1bc1c
Oscar Chavez <chavezoscar009@berkeley.edu>
2019-01-27 17:36:22 -0800

    Some error message for this code

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -1,8 +1,45 @@
 import ugradio
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+
+#READING IN ALL THE DATA WE HAVE COLLECTED
+data1 = np.load('data1.npy')
+data2 = np.load('data2.npy')
+data3 = np.load('data3.npy')
+data4 = np.load('data4.npy')
+data5 = np.load('data5.npy')
+data6 = np.load('data6.npy')
+data7 = np.load('data7.npy')
+data8 = np.load('data8.npy')
+data9 = np.load('data9.npy')
+
+print(data1[:13])
+
+#TAKING ONLY THE FIRST N FOR PLOTTING PURPOSES
+
+firstN = data1[:100]
+
+#PLOTTING CODE FOR FIRST N DATA POINTS
+
+plt.xlabel('Sample Observation')
+plt.ylabel('Data')
+plt.title('First N Data')
+plt.plot(firstN, 'o')
+plt.show()
 
 
-print('Wassup Compas')
+
+
+
+
+
+
+
+
+
+
+
 
 
